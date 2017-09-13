1798581b36a642f080958cab38c84f76c3cb3d5e
Unknown <mr.a.thewizard@gmail.com>
2017-09-14 01:41:25 +0530

    derived features , labels from digits dataset and initialized weights. Calculated sigmoid function

--- image_recognizer.py
+++ image_recognizer.py
@@ -1,8 +1,22 @@
 from sklearn import datasets
-import pylab as pl
+import matplotlib.pyplot as plt
+import numpy as np
+
+def calculate_sigmoid(z):
+    return (1/(1+np.exp(-z)))
+
+def calculate_z(weights, features, labels):
+    intercept = 0
+    z = np.dot(weights, features) + intercept
+    sigmoid_matrix = calculate_sigmoid(z)
+    print(sigmoid_matrix)
 def main():
     digits = datasets.load_digits()
-    print(digits.images.shape)
-    pl.imshow(digits.images[0],cmap = None)
+    features = np.array(digits.data, 'int16')
+    labels = np.array(digits.target,'int')
+    features = features.T
+    labels = labels.T
+    weights = np.ones((1,features.shape[0]))
+    calculate_z(weights, features, labels)
 if __name__ == '__main__':
     main()
