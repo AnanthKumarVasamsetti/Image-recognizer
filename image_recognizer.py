bf59ed7566f8cb5bb6328ead424497951766861d
AnanthKumarVasamsetti <mr.a.thewizard@gmail.com>
2017-09-14 15:16:31 +0530

    calculated resultant matrix with sigmoid function

--- image_recognizer.py
+++ image_recognizer.py
@@ -8,15 +8,25 @@ def calculate_sigmoid(z):
 def calculate_z(weights, features, labels):
     intercept = 0
     z = np.dot(weights, features) + intercept
-    sigmoid_matrix = calculate_sigmoid(z)
-    print(sigmoid_matrix)
+    print(features.shape)
+    resultant_matrix = calculate_sigmoid(z)
+    print(resultant_matrix.shape)
+    resultant_matrix = resultant_matrix-labels
+    print(resultant_matrix)
+    print(resultant_matrix.shape)
+
+
 def main():
     digits = datasets.load_digits()
+
     features = np.array(digits.data, 'int16')
     labels = np.array(digits.target,'int')
+
     features = features.T
     labels = labels.T
-    weights = np.ones((1,features.shape[0]))
+
+    weights = np.zeros((1,features.shape[0]))
     calculate_z(weights, features, labels)
+
 if __name__ == '__main__':
     main()
